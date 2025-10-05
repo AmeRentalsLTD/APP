995e69c39abcf00f5f477ce1549f98feba8bccbd
AmeRentalsLTD <contact@amerentals.co.uk>
2025-10-05 16:18:35 +0100

    Fix routing configuration to load web routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        using: function (): void {
+        then: function (): void {
             require base_path('routes/docs.php');
         },
     )
